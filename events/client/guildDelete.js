31ad74a673ca5bfd634846ae0a880f7573a491a3
Rabenherz112 <jeremy8373@gmail.com>
2022-11-26 17:03:25 +0100

    Removed Debug Information

--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -4,7 +4,6 @@ module.exports = {
 			if(!client || !client.db)
 			return;
 			let db = client.db;
-            console.log(`[GUILD] Left guild ${guild.name} (${guild.id})`);
 			await db.delete(`${guild.id}`);
 		} catch(e) {
 			// Ignore
