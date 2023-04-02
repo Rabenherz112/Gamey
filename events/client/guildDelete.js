d55ef51bf646165f910df567173c880891bb792a
Rabenherz112 <rabenherz@theravenhub.com>
2023-04-02 12:36:42 +0200

    Format

--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -1,11 +1,11 @@
 module.exports = {
 	"": async (client, guild) => {
 		try {
-			if(!client || !client.db)
-			return;
+			if (!client || !client.db)
+				return;
 			let db = client.db;
 			await db.delete(`${guild.id}`);
-		} catch(e) {
+		} catch (e) {
 			// Ignore
 		}
 	},
